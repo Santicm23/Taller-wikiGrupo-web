f19a6b3046d6ab2fed03a48043aebfcba9b178d6
Santicm23 <santicastrom@outlook.com>
2023-08-11 12:21:21 -0500

    cambiando la estructura a como el profesor pidio

---
+++ src/main/java/com/linx/wiki/view/WikiView.java
@@ -0,0 +1,23 @@
+package com.linx.wiki.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class WikiView {
+
+    @GetMapping("arquitectura")
+    public String paginaArquitectura() {
+        return "arquitectura";
+    }
+    @GetMapping("contactenos")
+    public String paginaContactenos() {
+        return "contactenos";
+    }
+    @GetMapping("descripcion")
+    public String paginaDescripcion() {
+        return "descripcion";
+    }
+}
